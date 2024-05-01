23a46a945e85e725ae9292fd5f27480fa0765940
Gerren Keith <gerren.keith23@gmail.com>
2024-05-01 11:54:40 -0500

    Game board
    created a basic gameBoard function to hold an array
    that represents the games board.

---
+++ script.js
@@ -0,0 +1,15 @@
+const gameBoard = () => {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for( let i = 0; i < rows; i++){
+        board[i] = [];
+        for( let j = 0; j < columns; j++){
+            board[i][j] = 0;
+        }
+    }
+    console.log(board)
+}
+
+gameBoard()
